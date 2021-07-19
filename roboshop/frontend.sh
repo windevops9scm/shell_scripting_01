df2a773afc85f4927558c2b29e3a4354201c6f53
windevops9scm <windevops9@gmail.com>
2021-07-19 23:23:14 +0530

    status changed

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 LOG=/temp/roboshop.log
 rm -f $LOG
-echo -e "installing nginx\t\t...\e[32mdone\e[0m"
+echo -e "installing nginx\t\t..."
 yum install nginx -y &>>LOG
+if [ $? -eq 0 ]
+then
+echo -e \e[32mdone\e[0m"
+else
+echo -e \e[31mfail\e[0m"
+fi
+
 echo -e "enabling nginx"
 systemctl enable nginx &>>LOG
 echo -e "starting nginx\t"

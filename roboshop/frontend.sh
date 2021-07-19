dc4aae60567a1517e1b0180d29fe728238e03edf
windevops9scm <windevops9@gmail.com>
2021-07-19 23:30:09 +0530

    status codes changed

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -2,7 +2,7 @@
 LOG=/temp/roboshop.log
 rm -f $LOG
 echo -n -e "installing nginx\t\t..."
-yum install nginx -y &>>LOG
+yyum install nginx -y &>>LOG
 if [ $? -eq 0 ]
 then
 echo -e "\e[32mdone\e[0m"
@@ -14,16 +14,16 @@ echo -e "enabling nginx"
 systemctl enable nginx &>>LOG
 if [ $? -eq 0 ]
 then
-echo -e "\e[32mdone\e[0m"
+echo -n -e "\e[32mdone\e[0m"
 else
-echo -e "\e[31mfail\e[0m"
+echo -n -e "\e[31mfail\e[0m"
 fi
 
 echo -e "starting nginx\t"
 systemctl start nginx &>>LOG
 if [ $? -eq 0 ]
 then
-echo -e "\e[32mdone\e[0m"
+echo -n -e "\e[32mdone\e[0m"
 else
-echo -e "\e[31mfail\e[0m"
+echo -n -e "\e[31mfail\e[0m"
 fi

4f5b1431b26088fc099b2256056000eebafba8ae
windevops9scm <windevops9@gmail.com>
2021-07-19 23:11:18 +0530

    frontend changed

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -3,5 +3,5 @@ echo -e "installing nginx\t\t...\e[31mdone\e[0m"
 yum install nginx -y
 echo -e "enabling nginx"
 systemctl enable nginx
-echo -e "starting nginx"
+echo -e "starting nginx\t"
 systemctl start nginx

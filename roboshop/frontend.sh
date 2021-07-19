ec5d52dd1eef3d910029aad85677392fd191b54d
windevops9scm <windevops9@gmail.com>
2021-07-19 23:59:42 +0530

    user authentication added to run the script

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -8,11 +8,18 @@ print "installing nginx"
 yum install nginx -y &>>LOG
 Status_check $?
 
-print "enabling nginx"
+print "  enabling nginx "
 systemctl enable nginx &>>LOG
 Status_check $?
 
-print "starting nginx"
+print "  starting nginx  "
 systemctl start nginx &>>LOG
 Status_check $?
+
+USER_ID=$(id -u)
+if [ $USER_ID -ne 0 ]
+then
+echo -e "\e you should be a root/sudo user to run this script"
+exit 2
+fi
 

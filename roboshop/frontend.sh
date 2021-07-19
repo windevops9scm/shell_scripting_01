29ccdc2ea185f3f12250b9bc37e15f9df0f5fa7c
windevops9scm <windevops9@gmail.com>
2021-07-19 23:39:03 +0530

    status checks

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 LOG=/temp/roboshop.log
 rm -f $LOG
-echo -n -e "installing nginx\t\t..."
-yyum install nginx -y &>>LOG
-if [ $? -eq 0 ]
+
+Status_check()
+{
+  if [ $? -eq 0 ]
 then
 echo -ne "\e[32mdone\e[0m"
 else
 echo -ne "\e[31mfail\e[0m"
 exit 1
 fi
+}
+
+echo -n -e "installing nginx\t\t..."
+yum install nginx -y &>>LOG
+Status_check $?
+
 
 echo -ne "enabling nginx\t\t"
 systemctl enable nginx &>>LOG
-if [ $? -eq 0 ]
-then
-echo -n -e "\e[32mdone\e[0m"
-else
-echo -n -e "\e[31mfail\e[0m"
-fi
+Status_check $?
+
 
 echo -ne "starting nginx\t\t"
 systemctl start nginx &>>LOG
-if [ $? -eq 0 ]
-then
-echo -e "\e[32mdone\e[0m"
-else
-echo -e "\e[31mfail\e[0m"
-fi
+Status_check $?
+

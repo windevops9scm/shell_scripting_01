7c2a80e3108a64e70cd3f661e55fcada6595aca2
windevops9scm <windevops9@gmail.com>
2021-07-21 22:02:04 +0530

    redis component listen address changed

--- roboshop/redis.sh
+++ roboshop/redis.sh
@@ -9,9 +9,9 @@ yum install redis -y --enablerepo=remi &>>$LOG
 STATUS_CHECK $?
 
 PRINT "update redis listen address"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf && /etc/redis/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf
 STATUS_CHECK $?
 
 PRINT "start redis service\t\t"
-systemctl enable redis &>>$LOG && systemctl start redis &>>$LOG
+systemctl enable redis &>>$LOG && systemctl restart redis &>>$LOG
 STATUS_CHECK $?

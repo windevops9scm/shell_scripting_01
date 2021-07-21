cda176c4c3f59bccde5e9cc2a173a097a55f522e
windevops9scm <windevops9@gmail.com>
2021-07-21 21:47:08 +0530

    redis component added

--- roboshop/redis.sh
+++ roboshop/redis.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 source common.sh
+PRINT "install redis repos"
+yum install epel-release yum-utils http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>>$LOG
+STATUS_CHECK $?
+
+PRINT "install redis\t"
+yum install redis -y --enable repo=remi >>$LOG
+STATUS_CHECK $?
+
+PRINT "update redis listen address"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf && /etc/redis/redis.conf
+STATUS_CHECK $?
+
+PRINT "start redis service\t"
+systemctl enable redis &>>$LOG && systemctl start redis &>>$LOG
+STATUS_CHECK $?

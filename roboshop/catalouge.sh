adf2ff2f0577d75d73cb7a3eb04a28e395de30c6
windevops9scm <windevops9@gmail.com>
2021-07-21 15:58:51 +0530

    catalouge added

--- roboshop/catalouge.sh
+++ roboshop/catalouge.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 source common.sh
+
+PRINT "install nodejs\t"
+yum install nodejs make gcc-c++ -y &>>LOG
+STATUS_CHECK $?
+
+PRINT "add roboshop application user"
+useradd roboshop &>>LOG
+STATUS_CHECK $?

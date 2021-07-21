012531d5e9fa9b00db7d18e6a40b2a2297ede5aa
windevops9scm <windevops9@gmail.com>
2021-07-21 21:23:12 +0530

    roboshop config file updated

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -13,9 +13,14 @@ cd /usr/share/nginx/html && rm -rf * &>>$LOG
 STATUS_CHECK $?
 PRINT "Extract frontend archive"
 unzip /tmp/frontend.zip &>>$LOG && mv frontend-main/* . &>> $LOG && mv static/* . &>>$LOG && rm -rf frontend-master static &>>$LOG
-STATUS_CHECK $
-PRINT "update roboshop config\t\t"
+STATUS_CHECK $?
+
+PRINT "copy roboshop config\t"
 mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG
+STATUS_CHECK $?
+
+PRINT "update roboshop config\t"
+sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
 STATUS_CHECK $?
 
 PRINT "enabling nginx \t\t"

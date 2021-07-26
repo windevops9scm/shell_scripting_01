92887c305363f73980e2e8d3bebdd236778fd03c
windevops9scm <windevops9@gmail.com>
2021-07-26 20:04:08 +0530

    mysql db added

--- roboshop/mysql.sh
+++ roboshop/mysql.sh
@@ -7,8 +7,8 @@ name=MySQL 5.7 Community Server
 baseurl=http://repo.mysql.com/yum/mysql-5.7-community/el/7/$basearch/
 enabled=1
 gpgcheck=0' > /etc/yum.repos.d/mysql.repo
-STAT_CHECK $?
+STATUS_CHECK $?
 
 PRINT "Install MySQL\t\t"
 yum install mysql-community-server -y &>>$LOG
-STAT_CHECK $?
+STATUS_CHECK $?

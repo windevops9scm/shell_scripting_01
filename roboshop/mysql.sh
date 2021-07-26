f4d915e51b6bdf1ae4aff204fa9c8a5b8b1dfc9a
windevops9scm <windevops9@gmail.com>
2021-07-26 20:49:16 +0530

    Loading schema

--- roboshop/mysql.sh
+++ roboshop/mysql.sh
@@ -19,6 +19,20 @@ STATUS_CHECK $?
 
 PRINT "reset MySQL default password\t\t"
 DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
+echo "show databases;" | mysql -uroot -pRoboShop@1 &>>$LOG
+if [ $? -ne 0 ]; then
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" | mysql --connect-expired-password -uroot -p${DEFAULT_PASSWORD} &>>$LOG
+fi
 STATUS_CHECK $?
 
+PRINT "uninstall MySQL password policy\t\t"
+echo "uninstall plugin validate_password" | mysql -uroot -pRoboShop@1 &>>$LOG
+STATUS_CHECK $?
+
+PRINT"Download schema"
+curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" &>>$LOG
+STATUS_CHECK $?
+
+PRINT"load schema"
+cd /tmp && unzip mysql.zip &>>$LOG && cd mysql-main && mysql  -uroot -pRoboShop@1 <shipping.sql &>>$LOG
+STATUS_CHECK $?

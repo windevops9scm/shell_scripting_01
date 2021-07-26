6a18a8dce7d16a327aa48eeffad36323bc297402
windevops9scm <windevops9@gmail.com>
2021-07-26 22:38:49 +0530

    systemd services updated

--- roboshop/common.sh
+++ roboshop/common.sh
@@ -46,7 +46,7 @@ chown roboshop:roboshop /home/roboshop -R &>>$LOG
 STATUS_CHECK $?
 }
 SETUP_SYSTEMD() {
-  PRINT "setup systemd file\t"
+  PRINT "setup systemd file \t"
 sed -i -e "s/MONGO_DNSNAME/mongodb.roboshop.internal/" -e "s/REDIS_ENDPOINT/redis.robpshop.internal/" -e "s/MONGO_ENDPOINT/mongodb.roboshop.internal/" -e "s/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/" -e "s/CARTENDPOINT/cart.roboshop.internal/"  -e 's/DBHOST/mysql.roboshop.internal/' -e 's/CARTHOST/cart.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal/' -e 's/AMQPHOST/rabbitmq.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service && mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
 STATUS_CHECK $?
 

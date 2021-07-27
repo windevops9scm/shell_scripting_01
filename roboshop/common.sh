a22b2d0bd6539d4f584d8463b89d23d40f97b3ba
windevops9scm <windevops9@gmail.com>
2021-07-27 19:11:40 +0530

    payment systemd updated

--- roboshop/common.sh
+++ roboshop/common.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 USER_ID=$(id -u)
-if [ $USER_ID -ne 0 ]
-then
-echo -e "\e[33myou should be a root/sudo user to run this script\e[0m"
-exit 2
-fi
-LOG=/tmp/roboshop.log
-rm -f $LOG
+  if [ $USER_ID -ne 0 ]
+  then
+  echo -e "\e[33myou should be a root/sudo user to run this script\e[0m"
+  exit 2
+  fi
+  LOG=/tmp/roboshop.log
+  rm -f $LOG
 
 STATUS_CHECK() {
   if [ $1 -eq 0 ]; then
@@ -22,53 +22,54 @@ PRINT() {
   echo -e "\n###########################\t$1\t###########################" &>>$LOG
   echo -n -e "$1\t\t..."
 }
+
 ADD_APPLICATION_USER() {
-PRINT "add roboshop application user \t"
-id roboshop &>>$LOG
- if [ $? -ne 0 ]; then
-    useradd roboshop &>>$LOG
- fi
-STATUS_CHECK $?
+  PRINT "add roboshop application user \t"
+  id roboshop &>>$LOG
+   if [ $? -ne 0 ]; then
+      useradd roboshop &>>$LOG
+   fi
+  STATUS_CHECK $?
 }
 
-DOWNLOAD_APP_CODE()
-{
+DOWNLOAD_APP_CODE() {
   PRINT "Download application code\t"
-curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>$LOG
-STATUS_CHECK $?
-PRINT "extract downloaded code\t"
-cd /home/roboshop && unzip -o /tmp/${COMPONENT}.zip &>>$LOG && rm -rf ${COMPONENT} && mv ${COMPONENT}-main ${COMPONENT}
-STATUS_CHECK $?
+  curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>$LOG
+  STATUS_CHECK $?
+  PRINT "extract downloaded code\t"
+  cd /home/roboshop && unzip -o /tmp/${COMPONENT}.zip &>>$LOG && rm -rf ${COMPONENT} && mv ${COMPONENT}-main ${COMPONENT}
+  STATUS_CHECK $?
 }
 PERM_FIX() {
   PRINT "Fix Application Permissions"
-chown roboshop:roboshop /home/roboshop -R &>>$LOG
-STATUS_CHECK $?
+  chown roboshop:roboshop /home/roboshop -R &>>$LOG
+  STATUS_CHECK $?
 }
-SETUP_SYSTEMD() {
-  PRINT "setup systemd file \t"
-#sed -i -e "s/MONGO_DNSNAME/mongodb.roboshop.internal/" -e "s/REDIS_ENDPOINT/redis.robpshop.internal/" -e "s/MONGO_ENDPOINT/mongodb.roboshop.internal/" -e "s/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/" -e "s/CARTENDPOINT/cart.roboshop.internal/"  -e 's/DBHOST/mysql.roboshop.internal/' -e 's/CARTHOST/cart.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal/' -e 's/AMQPHOST/rabbitmq.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service && mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
-sed -i -e "s/MONGO_DNSNAME/mongodb.roboshop.internal/" -e "s/REDIS_ENDPOINT/redis.roboshop.internal/" -e "s/MONGO_ENDPOINT/mongodb.roboshop.internal/" -e "s/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/" -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal/' -e 's/CARTHOST/cart.roboshop.internal/' -e 's/AMQPHOST/rabbitmq.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service && mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
-STATUS_CHECK $?
 
-PRINT "start ${COMPONENT} service\t "
-systemctl daemon-reload &>>$LOG && systemctl restart ${COMPONENT} &>>$LOG && systemctl enable ${COMPONENT} &>>$LOG
-STATUS_CHECK $?
+SETUP_SYSTEMD() {
+   PRINT "setup systemd file \t"
+  #sed -i -e "s/MONGO_DNSNAME/mongodb.roboshop.internal/" -e "s/REDIS_ENDPOINT/redis.robpshop.internal/" -e "s/MONGO_ENDPOINT/mongodb.roboshop.internal/" -e "s/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/" -e "s/CARTENDPOINT/cart.roboshop.internal/"  -e 's/DBHOST/mysql.roboshop.internal/' -e 's/CARTHOST/cart.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal/' -e 's/AMQPHOST/rabbitmq.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service && mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
+  sed -i -e "s/MONGO_DNSNAME/mongodb.roboshop.internal/" -e "s/REDIS_ENDPOINT/redis.roboshop.internal/" -e "s/MONGO_ENDPOINT/mongodb.roboshop.internal/" -e "s/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/" -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal/' -e 's/CARTHOST/cart.roboshop.internal/' -e 's/AMQPHOST/rabbitmq.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service && mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
+  STATUS_CHECK $?
+
+  PRINT "start ${COMPONENT} service\t "
+  systemctl daemon-reload &>>$LOG && systemctl restart ${COMPONENT} &>>$LOG && systemctl enable ${COMPONENT} &>>$LOG
+  STATUS_CHECK $?
 }
 
 NODEJS() {
-PRINT "install nodejs\t\t"
-yum install nodejs make gcc-c++ -y &>>$LOG
-STATUS_CHECK $?
+  PRINT "install nodejs\t\t"
+  yum install nodejs make gcc-c++ -y &>>$LOG
+  STATUS_CHECK $?
 
-ADD_APPLICATION_USER
-DOWNLOAD_APP_CODE
+  ADD_APPLICATION_USER
+  DOWNLOAD_APP_CODE
 
-PRINT "install nodejs dependencies"
-cd /home/roboshop/${COMPONENT} && npm install --unsafe-perm &>>$LOG
-STATUS_CHECK $?
-PERM_FIX
-SETUP_SYSTEMD
+  PRINT "install nodejs dependencies"
+  cd /home/roboshop/${COMPONENT} && npm install --unsafe-perm &>>$LOG
+  STATUS_CHECK $?
+  PERM_FIX
+  SETUP_SYSTEMD
 }
 
 JAVA() {
@@ -94,21 +95,18 @@ PYTHON3() {
   ADD_APPLICATION_USER
   DOWNLOAD_APP_CODE
 
-PRINT "Install python dependencies"
-cd /home/roboshop/${COMPONENT} &>>$LOG && pip3 install -r requirements.txt &>>$LOG
-STATUS_CHECK $?
+  PRINT "Install python dependencies"
+  cd /home/roboshop/${COMPONENT} &>>$LOG && pip3 install -r requirements.txt &>>$LOG
+  STATUS_CHECK $?
 
   PRINT "update service configuration "
   userID=$(id -u roboshop)
   groupID=$(id -g roboshop)
   sed -i -e "/uid/ c uid=${userID}" -e "/gid/ c gid=${groupID}" payment.ini &>>$LOG
   STATUS_CHECK $?
+
    PERM_FIX
    SETUP_SYSTEMD
 
 }
 
-# mv /home/roboshop/payment/systemd.service /etc/systemd/system/payment.service
-# systemctl daemon-reload
-# systemctl enable payment
-# systemctl start payment   payment.ini

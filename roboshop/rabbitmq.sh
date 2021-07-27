a9e97048792d83a3a0fe2539b7db788ee9e5a90d
windevops9scm <windevops9@gmail.com>
2021-07-27 17:57:24 +0530

    rabbitmq component added

--- roboshop/rabbitmq.sh
+++ roboshop/rabbitmq.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 source common.sh
+
+PRINT "Install Erlang \t"
+yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>>$LOG
+STATUS_CHECK $?
+#curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
+# yum install rabbitmq-server -y
+
+# systemctl enable rabbitmq-server
+# systemctl start rabbitmq-server
+
+# rabbitmqctl add_user roboshop roboshop123
+# rabbitmqctl set_user_tags roboshop administrator
+# rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+
+#create rabbitmq dns

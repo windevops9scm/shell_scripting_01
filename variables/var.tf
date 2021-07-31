2ba14bc3b331ce162d983d6949bbbceffb0738e0
windevops9scm <windevops9@gmail.com>
2021-07-31 22:52:59 +0530

    terraform datatypes intro

--- variables/var.tf
+++ variables/var.tf
@@ -11,3 +11,28 @@ variable "sample2" {
 output "sample2" {
   value = var.sample2
 }
+variable "string" {
+  default = "hello"
+}
+
+variable "number" {
+  default = 10
+}
+variable "boolean" {
+  default = false
+}
+
+output "string" {
+  value = var.string
+}
+
+output "number" {
+  value = var.number
+}
+
+output "boolean" {
+  value = var.boolean
+}
+output "data_types" {
+  value = String = ${var.string}
+}

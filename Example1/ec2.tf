ab9a9d43703b9adef83e93aa526c0bfd7847052b
windevops9scm <windevops9@gmail.com>
2021-08-01 00:09:01 +0530

    terraform ec2 instances

--- Example1/ec2.tf
+++ Example1/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "sample" {
-  ami            ="ami-074df373d6bafa625  "
+  ami            ="ami-074df373d6bafa625"
   instance_type  = "t2.micro"
   vpc_security_group_ids = []
 

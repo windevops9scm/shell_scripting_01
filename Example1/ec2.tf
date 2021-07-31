12425dd31a14b922b33bbc0b26027abca03e3d5a
windevops9scm <windevops9@gmail.com>
2021-07-31 23:52:46 +0530

    terraform ec2 instances

---
+++ Example1/ec2.tf
@@ -0,0 +1,39 @@
+resource "aws_instance" "sample" {
+  ami            ="ami-074df373d6bafa625  "
+  instance_type  = "t2.micro"
+  vpc_security_group_ids = []
+
+tags = {
+  Name = "sample"
+}
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_sss"
+  description = "ssh"
+
+
+  ingress {
+    description      = "ssh"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+output "sg_attributes" {
+  value = aws_security_group.allow_ssh
+}
--- variables/terraform.tfvars
+++ variables/terraform.tfvars
@@ -1,1 +1,3 @@
 Fruits=["Apple", "Grapes"]
+
+

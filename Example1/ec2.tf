d5f84ee6304c3e718a8e5a25e677c3bac1b1217c
windevops9scm <windevops9@gmail.com>
2021-08-01 00:13:16 +0530

    terraform ec2 instances creations

--- Example1/ec2.tf
+++ Example1/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "sample" {
   ami            ="ami-074df373d6bafa625"
   instance_type  = "t2.micro"
-  vpc_security_group_ids = []
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
 tags = {
   Name = "sample"
@@ -34,8 +34,8 @@ resource "aws_security_group" "allow_ssh" {
     Name = "allow_ssh"
   }
 }
-output "sg_attributes" {
-  value = aws_security_group.allow_ssh
+output "ec2_attributes" {
+  value = aws_instance.sample
 }
 provider "aws" {
   region = "us-east-1"

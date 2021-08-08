e25c17a53ed5dee473f4f5884c2ceb0ae1727bfa
windevops9scm <windevops9@gmail.com>
2021-08-08 18:05:29 +0530

    few change made folder structure

--- terraform/Example1/ec2.tf
+++ terraform/Example1/ec2.tf
@@ -27,7 +27,7 @@ resource "aws_security_group" "allow_ssh" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+
   }
 
   tags = {

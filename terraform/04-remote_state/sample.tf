e712ae269331bc4bd40374f472017b70ebfa5be4
windevops9scm <windevops9@gmail.com>
2021-08-09 17:00:08 +0530

    s3 bucket added

---
+++ terraform/04-remote_state/sample.tf
@@ -0,0 +1,20 @@
+resource "aws_instance" "sample" {
+  ami = "ami-074df373d6bafa625"
+  instance_type = "t2.micro"
+  }
+
+tags = {
+  Name = "sample"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+terraform {
+  backend "s3" {
+    bucket = "terraform-d34"
+    key = "sample/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+

8aa81eb4858974b8a24e872e4cfb6286a8500168
windevops9scm <windevops9@gmail.com>
2021-08-09 19:45:15 +0530

    testing ec2

--- terraform/03-Example1/ec2.tf
+++ terraform/03-Example1/ec2.tf
@@ -1,3 +1,14 @@
+resource "aws_instance" "sample" {
+  ami            ="ami-074df373d6bafa625"
+  instance_type  = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  tags = {
+    Name = "sample"
+  }
+}
+
+
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
   description = "alow_ssh"

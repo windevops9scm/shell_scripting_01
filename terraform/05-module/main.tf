c274d49b973d6414ef9549afb210afc32bfbb4d8
windevops9scm <windevops9@gmail.com>
2021-08-14 08:22:48 +0530

    modules used for aws creation

---
+++ terraform/05-module/main.tf
@@ -0,0 +1,12 @@
+module "sg" {
+  source     = "./sg"
+}
+
+module "ec2" {
+  depends_on = [module.sg]
+  source     = "./ec2"
+  SG_ID      = module.sg.SG_ID
+}
+provider "aws" {
+  region     = "us-east-1"
+}

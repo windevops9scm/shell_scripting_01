45d416447c610b59c8aae197cec4a471fc2164e7
windevops9scm <windevops9@gmail.com>
2021-08-14 08:41:16 +0530

    testing modules

---
+++ terraform/05-modules/ec2/ec2.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "sample" {
+  ami                    ="ami-074df373d6bafa625"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [var.SG_ID]
+
+  tags                   = {
+    Name                 = "sample"
+  }
+}
+
+variable "SG_ID" {}
+
+output "PRIVATE_IP" {
+  value                  = aws_instance.sample.private_ip
+}
---
+++ terraform/05-modules/sg/sg.tf
@@ -0,0 +1,30 @@
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "alow_ssh"
+
+
+  ingress {
+    description      = "ssh"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+
+  tags               = {
+    Name             = "allow_ssh"
+  }
+}
+
+output "SG_ID" {
+  value = aws_security_group.allow_ssh.id
+}

295c2e54dccad9b601a24c70490e5e83d7fd365b
windevops9scm <windevops9@gmail.com>
2021-08-14 16:30:16 +0530

    terraform conditions

--- terraform/06-conditions/sample.tf
+++ terraform/06-conditions/sample.tf
@@ -1,14 +1,18 @@
 resource "aws_instance" "sample" {
-  count = 0
+  #count          = 0
   ami            ="ami-074df373d6bafa625"
-  instance_type  = "t2.micro"
+  instance_type  = var.TYPE == null ? "t3.micro" : var.TYPE
   #vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
-  tags = {
-    Name = "sample"
+  tags           = {
+    Name         = "sample"
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region        = "us-east-1"
 }
+
+
+variable "TYPE" {
+  default       = null

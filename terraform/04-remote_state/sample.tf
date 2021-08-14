4380823fc446d42bb92d6a26a988b35d9df021cb
windevops9scm <windevops9@gmail.com>
2021-08-14 07:42:57 +0530

    dynamodb and lockid

--- terraform/04-remote_state/sample.tf
+++ terraform/04-remote_state/sample.tf
@@ -1,19 +1,20 @@
 resource "aws_instance" "sample" {
-  ami           = "ami-074df373d6bafa625"
-  instance_type = "t2.micro"
-  tags          = {
+  ami             = "ami-074df373d6bafa625"
+  instance_type   = "t2.micro"
+  tags            = {
     Name          = "sample"
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region          = "us-east-1"
 }
 
 terraform {
   backend "s3" {
-    bucket  = "terraform-d34"
-    key     = "sample/terraform.tfstate"
-    region  = "us-east-1"
+    bucket        = "terraform-d34"
+    key           = "sample/terraform.tfstate"
+    region        = "us-east-1"
+    dynamodb_table ="terraform"
   }
 }

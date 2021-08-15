d5a34c9ceaef4459cbedb62e3006f648505e33e2
windevops9scm <windevops9@gmail.com>
2021-08-15 09:16:03 +0530

    terraform data sources

---
+++ terraform/07-data-sources/data.tf
@@ -0,0 +1,17 @@
+data "aws_ami" "example" {
+  most_recent       = true
+  name_regex       = "^Centos*"
+  owners            = ["973714476881"]
+}
+  output "amis" {
+    value           = data.aws_ami.example.id
+  }
+
+provider "aws" {
+  region = "us-east-1"
+}
+//
+//  filter {
+//    name   = "name"
+//    values = ["myami-*"]
+//  }
